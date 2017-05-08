7099c2bebf29366a724f1183ad79dde64848b9bc
Josh <joshm998@gmail.com>
2017-05-08 12:41:45 +0930

    fixed cancel button issue and added delete

---
+++ VendorMaintenance/frmStateModify.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace VendorMaintenance
+{
+    partial class frmStateModify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStateName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPostLast = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStateCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtStateName
+            // 
+            this.txtStateName.Location = new System.Drawing.Point(12, 81);
+            this.txtStateName.Name = "txtStateName";
+            this.txtStateName.Size = new System.Drawing.Size(226, 20);
+            this.txtStateName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "State Name";
+            // 
+            // txtPost
+            // 
+            this.txtPost.Location = new System.Drawing.Point(12, 120);
+            this.txtPost.Name = "txtPost";
+            this.txtPost.Size = new System.Drawing.Size(100, 20);
+            this.txtPost.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Postcode";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Postcode";
+            // 
+            // txtPostLast
+            // 
+            this.txtPostLast.Location = new System.Drawing.Point(12, 160);
+            this.txtPostLast.Name = "txtPostLast";
+            this.txtPostLast.Size = new System.Drawing.Size(100, 20);
+            this.txtPostLast.TabIndex = 5;
+           // this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "State Code";
+            // 
+            // txtStateCode
+            // 
+            this.txtStateCode.Location = new System.Drawing.Point(12, 42);
+            this.txtStateCode.Name = "txtStateCode";
+            this.txtStateCode.Size = new System.Drawing.Size(100, 20);
+            this.txtStateCode.TabIndex = 9;
+            // 
+            // frmStateModify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 235);
+            this.Controls.Add(this.txtStateCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPostLast);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStateName);
+            this.Name = "frmStateModify";
+            this.Text = "Modify State";
+            this.Load += new System.EventHandler(this.frmStateModify_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStateName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPostLast;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStateCode;
+    }
+}
---
+++ VendorMaintenance/frmStateModify.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+namespace VendorMaintenance
+{
+    public partial class frmStateModify : Form
+    {
+        public frmStateModify()
+        {
+            InitializeComponent();
+        }
+
+        public bool addState;
+        public State state;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmStateModify_Load(object sender, EventArgs e)
+        {
+            if (addState)
+            {
+                this.Text = "Add State";
+            }
+            else
+            {
+                this.Text = "Modify Stater";
+                this.DisplayStateData();
+            }
+        }
+
+        private void DisplayStateData()
+        {
+            txtStateCode.Text = state.StateCode;
+            txtStateName.Text = state.StateName;
+            txtPost.Text = Convert.ToString(state.FirstZipCode);
+            txtPostLast.Text = Convert.ToString(state.LastZipCode);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                if (addState)
+                {
+                    state = new State();
+                    this.PutStateData(state);
+                    DataContext.payables.States.InsertOnSubmit(state);
+                    try
+                    {
+                        DataContext.payables.SubmitChanges();
+                        this.DialogResult = DialogResult.OK;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, ex.GetType().ToString());
+                    }
+                }
+                else
+                {
+                    this.PutStateData(state);
+                    try
+                    {
+                        DataContext.payables.SubmitChanges();
+                        this.DialogResult = DialogResult.OK;
+                    }
+                    catch (ChangeConflictException)
+                    {
+                        DataContext.GetCurrentValues();
+                        if (DataContext.vendorDeleted)
+                        {
+                            MessageBox.Show("Another user has deleted " +
+                                "that vendor.", "Database Error");
+                            this.DialogResult = DialogResult.Abort;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Another user has updated " +
+                                "that vendor.", "Database Error");
+                            this.DialogResult = DialogResult.Retry;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, ex.GetType().ToString());
+                    }
+                }
+
+
+            }
+        }
+
+        private void PutStateData(State state )
+        {
+            state.StateName = txtStateName.Text;
+            state.FirstZipCode = Convert.ToInt32(txtPost.Text);
+            state.LastZipCode = Convert.ToInt32(txtPostLast.Text);
+            state.StateCode = txtStateCode.Text;
+        }
+
+        private bool IsValidData()
+        {
+            if (Validator.IsPresent(txtStateName) &&
+                Validator.IsPresent(txtPostLast) &&
+                Validator.IsPresent(txtStateName))
+                return true;
+
+            else
+                return false;
+
+        }
+    }
+}
